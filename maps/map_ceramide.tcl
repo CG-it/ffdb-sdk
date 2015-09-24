aee6d1d669ea7e9119be1ef68abf05fa3bd7ee90
Chris MacDermaid <chris.macdermaid@gmail.com>
2015-09-24 10:22:46 -0400

    Added CER240 and CER482 mappings

--- maps/map_ceramide.tcl
+++ maps/map_ceramide.tcl
@@ -294,41 +294,381 @@ proc ::CGtools::map_ceramide {} {
         17
     }
 
+    ##CER482
+    set map([list map CER482]) {
+	{O1 HO1 C1S H1S H1T}
+	{C2S H2S NF HNF}
+	{C3S H3S O3 HO3}
+	{C4S H4S C5S H5S}
+	{C6S H6S H6T C7S H7S H7T C8S H8S H8T}
+	{C9S H9S H9T C10S H10S H10T C11S H11S H11T}
+	{C12S H12S H12T C13S H13S H13T C14S H14S H14T}
+	{C15S H15S H15T C16S H16S H16T}
+	{C17S H17S H17T C18S H18S H18T H18U}
+	{C1F OF}
+	{C2F H2F H2G C3F H3F H3G C4F H4F H4G}
+	{C5F H5F H5G C6F H6F H6G C7F H7F H7G}
+	{C8F H8F H8G C9F H9F H9G C10F H10F H10G}
+	{C11F H11F H11G C12F H12F H12G C13F H13F H13G}
+	{C14F H14F H14G C15F H15F H15G C16F H16F H16G}
+	{C17F H17F H17G C18F H18F H18G C19F H19F H19G}
+	{C20F H20F H20G C21F H21F H21G C22F H22F H22G}
+	{C23F H23F H23G C24F H24F H24G C25F H25F H25G}
+	{C26F H26F H26G C27F H27F H27G C28F H28F H28G}
+	{C29F H29F H29G C30F H30F H30G}
+	{C31F O30F O31F C32F H32F H32G}	
+	{C33F H33F H33G C34F H34F H34G C35F H35F H35G}
+	{C36F H36F H36G C37F H37F H37G C38F H38F H38G}
+	{C39F H39F C40F H40F C41F H41F H41G}
+	{C42F H42F C43F H43F}
+	{C44F H44F H44G C45F H45F H45G C46F H46F H46G}
+	{C47F H47F H47G C48F H48F H48G H48H}
+    }
+
+    set map([list type CER482])    {
+	SER 
+	N
+	SER
+	CMD2
+	CM
+	CM
+	CM
+	CM2
+	CT2
+	O
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM2
+	EST1
+	CM
+	CM
+	CMD3
+	CMD2
+	CM
+	CT2
+    }
+
+    set map([list name CER482])    {
+	OH1
+	N
+	OH2
+	CS3
+	CS4
+	CS5
+	CS6
+	CS7
+	CS8
+	OF1
+	CF2
+	CF3
+	CF4
+	CF5
+	CF6
+	CF7
+	CF8
+	CF9
+	CF10
+	CF11
+	OF12
+	CF13
+	CF14
+	CF15
+	CF16
+	CF17
+	CF18
+    }
+
+    set map([list charge CER482])  {
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+    }
+
+    set map([list mass CER482])    {
+	31.0344
+	28.0340
+	30.0264
+	26.0380
+	43.0890
+	42.0810
+	42.0810
+	28.0540
+	29.0620
+	28.0100
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	41.0730
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	28.0540
+	58.0368
+	42.0810
+	42.0810
+	40.0650
+	26.0380
+	42.0810
+	29.0620
+    }
+
+    set map([list bonds CER482]) {
+	{OH1 N} {OH2 N} {OH2 CS3}
+	{CS3 CS4} {CS4 CS5} {CS5 CS6}
+	{CS6 CS7} {CS7 CS8} {N OF1}
+	{OF1 CF2} {CF2 CF3} {CF3 CF4} {CF4 CF5}
+	{CF5 CF6} {CF6 CF7} {CF7 CF8}
+	{CF8 CF9} {CF9 CF10} {CF10 CF11}
+	{CF11 OF12} {OF12 CF13} {CF13 CF14}
+	{CF14 CF15} {CF15 CF16} {CF16 CF17}
+	{CF17 CF18}
+    }
+
+    set map([list angles CER482]) {
+	{auto}
+    }
+
+    set map([list atomicnumber CER482]) {
+	17
+	15
+	16
+	14
+	25
+	24
+	24
+	16
+	17
+	14
+	24
+	24
+	24
+	24
+	23
+	24
+	24
+	24
+	24
+	16
+	30
+	24
+	24
+	22
+	14
+	24
+	17
+    }
+
 
     # +------+
     # | CER2 |
     # +------+
 
+    ##TODO: FIX
+    
     set map([list map CER2]) {
-      {O1 HO1 C1 HA HB}
-      {C2 HS N21 HN}
-      {C3 HX O3 HO}
-      {C34 H4X C35 H5X}
-      {C36 H6X H6Y C37 H7X H7Y C38 H8X H8Y}
-      {C39 H9X H9Y C310 H10X H10Y C311 H11X H11Y}
-      {C312 H12X H12Y C313 H13X H13Y C314 H14X H14Y}
-      {C315 H15X H15Y C316 H16X H16Y}
-      {C317 H17X H17Y C318 H18X H18Y H18Z}
-      {C21 O22}
-      {C22 H2R H2S C23 H3R H3S C24 H4R H4S}
-      {C25 H5R H5S C26 H6R H6S C27 H7R H7S}
-      {C28 H8R H8S C29 H9R H9S C210 H10R H10S}
-      {C211 H11R H11S C212 H12R H12S C213 H13R H13S}
-      {C214 H14R H14S C215 H15R H15S C216 H16R H16S}
-      {C217 H17R H17S C218 H18R H18S C219 H19R H19S}
-      {C220 H20R H20S C221 H21R H21S C222 H22R H22S}
-      {C223 H23R H23S C224 H24R H24S H24T}
+	{O1 HO1 C1 HA HB}
+	{C2 HS N21 HN}
+	{C3 HX O3 HO}
+	{C34 H4X C35 H5X}
+	{C6S H6S H6T C7S H7S H7T C8S H8S H8T}
+	{C9S H9S H9T C10S H10S H10T C11S H11S H11T}
+	{C12S H12S H12T C13S H13S H13T C14S H14S H14T}
+	{C15S H15S H15T C16S H16S H16T}
+	{C17S H17S H17T C18S H18S H18T H18U}
+	{C21 O22}
+	{C22 H2R H2S C23 H3R H3S C24 H4R H4S}
+	{C25 H5R H5S C26 H6R H6S C27 H7R H7S}
+	{C28 H8R H8S C29 H9R H9S C210 H10R H10S}
+	{C211 H11R H11S C212 H12R H12S C213 H13R H13S}
+	{C214 H14R H14S C215 H15R H15S C216 H16R H16S}
+	{C217 H17R H17S C218 H18R H18S C219 H19R H19S}
+	{C220 H20R H20S C221 H21R H21S C222 H22R H22S}
+	{C223 H23R H23S C224 H24R H24S H24T}
     }
 
     set map([list type CER2])    {
-        SER 
-        N
-        SER
-        CMD2
-        CM
-        CM
-        CM
-        CM2
+	SER 
+	N
+	SER
+	CMD2
+	CM
+	CM
+	CM
+	CM2
+	CT2
+	O
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CM
+	CT2
+    }
+
+    set map([list name CER2])    {
+	OH1
+	N
+	OH2
+	CS3
+	CS4
+	CS5
+	CS6
+	CS7
+	CS8
+	OA1
+	CA2
+	CA3
+	CA4
+	CA5
+	CA6
+	CA7
+	CA8
+	CA9
+    }
+
+    set map([list charge CER2])  {
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+	0.0
+    }
+
+    set map([list mass CER2])    {
+	31.0344
+	28.0340
+	30.0264
+	26.0380
+	42.0810
+	42.0810
+	42.0810
+	28.0540
+	29.0620
+	28.0100
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	29.0620
+    }
+
+    set map([list bonds CER2]) {
+	{OH1 N} {OH2 N} {OH2 CS3}
+	{CS3 CS4} {CS4 CS5} {CS5 CS6}
+	{CS6 CS7} {CS7 CS8} {N OA1}
+	{OA1 CA2} {CA2 CA3} {CA3 CA4} {CA4 CA5}
+	{CA5 CA6} {CA6 CA7} {CA7 CA8}
+	{CA8 CA9}
+    }
+
+    set map([list angles CER2]) {
+	{auto}
+    }
+
+    set map([list atomicnumber CER2]) {
+	17
+	15
+	16
+	14
+	24
+	24
+	24
+	16
+	17
+	14
+	24
+	24
+	24
+	24
+	24
+	24
+	24
+	17
+    }
+
+    ##CER240
+    
+    set map([list map CER240]) {
+	{O1 HO1 C1S H1S H1T}
+	{C2S H2S NF HNF}
+	{C3S H3S O3 HO3}
+	{C4S H4S C5S H5S}
+	{C6S H6S H6T C7S H7S H7T C8S H8S H8T}
+	{C9S H9S H9T C10S H10S H10T C11S H11S H11T}
+	{C12S H12S H12T C13S H13S H13T C14S H14S H14T}
+	{C15S H15S H15T C16S H16S H16T}
+	{C17S H17S H17T C18S H18S H18T H18U}
+	{C1F OF}
+	{C2F H2F H2G C3F H3F H3G C4F H4F H4G}
+	{C5F H5F H5G C6F H6F H6G C7F H7F H7G}
+	{C8F H8F H8G C9F H9F H9G C10F H10F H10G}
+	{C11F H11F H11G C12F H12F H12G C13F H13F H13G}
+	{C14F H14F H14G C15F H15F H15G C16F H16F H16G}
+	{C17F H17F H17G C18F H18F H18G C19F H19F H19G}
+	{C20F H20F H20G C21F H21F H21G C22F H22F H22G}
+	{C23F H23F H23G C24F H24F H24G H24H}	
+    }
+
+    set map([list type CER240])    {
+	SER 
+	N
+	SER
+	CMD2
+	CM
+	CM
+	CM
+	CM2
         CT2
         O
         CM
@@ -341,7 +681,7 @@ proc ::CGtools::map_ceramide {} {
         CT2
     }
 
-    set map([list name CER2])    {
+    set map([list name CER240])    {
         OH1
         N
         OH2
@@ -351,18 +691,18 @@ proc ::CGtools::map_ceramide {} {
         CS6
         CS7
         CS8
-        OA1
-        CA2
-        CA3
-        CA4
-        CA5
-        CA6
-        CA7
-        CA8
-        CA9
+        OF1
+        CF2
+        CF3
+        CF4
+        CF5
+        CF6
+        CF7
+        CF8
+        CF9
     }
 
-    set map([list charge CER2])  {
+    set map([list charge CER240])  {
         0.0
         0.0
         0.0
@@ -383,41 +723,41 @@ proc ::CGtools::map_ceramide {} {
         0.0
     }
 
-    set map([list mass CER2])    {
-       31.0344
-       28.0340
-       30.0264
-       26.0380
-       42.0810
-       42.0810
-       42.0810
-       28.0540
-       29.0620
-       28.0100
-       42.0810
-       42.0810
-       42.0810
-       42.0810
-       42.0810
-       42.0810
-       42.0810
-       29.0620
+    set map([list mass CER240])    {
+	31.0344
+	28.0340
+	30.0264
+	26.0380
+	42.0810
+	42.0810
+	42.0810
+	28.0540
+	29.0620
+	28.0100
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	42.0810
+	29.0620
     }
 
-    set map([list bonds CER2]) {
-         {OH1 N} {OH2 N} {OH2 CS3}
-         {CS3 CS4} {CS4 CS5} {CS5 CS6}
-         {CS6 CS7} {CS7 CS8} {N OA1}
-         {OA1 CA2} {CA2 CA3} {CA3 CA4} {CA4 CA5}
-         {CA5 CA6} {CA6 CA7} {CA7 CA8}
-         {CA8 CA9}
+    set map([list bonds CER240]) {
+	{OH1 N} {OH2 N} {OH2 CS3}
+	{CS3 CS4} {CS4 CS5} {CS5 CS6}
+	{CS6 CS7} {CS7 CS8} {N OF1}
+	{OF1 CF2} {CF2 CF3} {CF3 CF4} {CF4 CF5}
+	{CF5 CF6} {CF6 CF7} {CF7 CF8}
+	{CF8 CF9}
     }
 
-    set map([list angles CER2]) {
+    set map([list angles CER240]) {
         {auto}
     }
 
-    set map([list atomicnumber CER2]) {
+    set map([list atomicnumber CER240]) {
         17
         15
         16
@@ -445,5 +785,5 @@ proc ::CGtools::map_ceramide {} {
 ::CGtools::map_ceramide
 
 if { $::CGtools::maptest } {
-    ::CGtools::map_stats {CER1 CER2}
+    ::CGtools::map_stats {CER1 CER2 CER240 CER482}
 }
